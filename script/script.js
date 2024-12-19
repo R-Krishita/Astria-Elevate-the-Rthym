dd0f2676565d415ed31f91913e87fc0354858ad7
RKrishita <krishitaravat12345@gmail.com>
2024-12-20 00:23:22 +0530

    getSongs function

--- script/script.js
+++ script/script.js
@@ -1,17 +1,38 @@
-function scrollLeft() {
-    const container = document.querySelector('.scroll-content');
-    container.scrollBy({
-        top: 0,
-        left: -100, // Adjust the value for scroll distance
-        behavior: 'smooth'
-    });
-}
+// function scrollLeft() {
+//     const container = document.querySelector('.scroll-content');
+//     container.scrollBy({
+//         top: 0,
+//         left: -100, // Adjust the value for scroll distance
+//         behavior: 'smooth'
+//     });
+// }
 
-function scrollRight() {
-    const container = document.querySelector('.scroll-content');
-    container.scrollBy({
-        top: 0,
-        left: 100, // Adjust the value for scroll distance
-        behavior: 'smooth'
-    });
+// function scrollRight() {
+//     const container = document.querySelector('.scroll-content');
+//     container.scrollBy({
+//         top: 0,
+//         left: 100, // Adjust the value for scroll distance
+//         behavior: 'smooth'
+//     });
+// }
+
+// function to get songs (we are doing this bcz it is client side scripting. when we do backend coding we take data using api)
+
+async function getSongs(){
+    let a =await fetch("http://127.0.0.1:3000/songs/")
+    let response =await a.text();
+    // console.log(response)
+    let element = document.createElement("div")
+    element.innerHTML  = response;
+    let anchor = element.getElementsByTagName("a")
+    console.log(anchor)
+    let songs = []
+    for (let index = 0; index < anchor.length; index++) {
+        const element = anchor[index];
+            if(element.href.endsWith(".mp3")){
+                songs.push(element.href)
+        }
+    }
+    return songs
 }
+getSongs()
